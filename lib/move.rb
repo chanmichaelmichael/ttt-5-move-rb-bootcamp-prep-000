dd70d1e685d3283cece1d371be197ec842be0051
Michael Michael <michael.ta.chan@gmail.com>
2019-04-16 13:00:36 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(move)
+  move = move.strip.chomp
+  move = Integer(move) - 1 #convert to integer
+end
+
+def move(board,position,character)
+  board[position] = character
+end
